941cf4fd0c4838672ae32a08d029c4520158afa2
Parker Brown <brownpt98@gmail.com>
2024-11-04 20:26:20 -0600

    Update decorator options

--- fetch_rss_articles.py
+++ fetch_rss_articles.py
@@ -18,15 +18,14 @@ def get_feed_articles(url: str) -> list:
     content = parser.parse(response.text).dict_plain()
     return [{"title": item["title"], "url": item["links"][0], "author": item["author"], "published_at": item["pub_date"]} for item in content["channel"]["items"]]
 
-@task
+@task(persist_result=False)
 def save_articles(feed_id: int, articles: list):
     db_context = SqlAlchemyConnector.load("pg-local")
     with db_context as session:
         session.execute_many("INSERT INTO rss_articles (feed_id, title, url, author, published_at) VALUES (:feed_id, :title, :url, :author, :published_at)", [{"feed_id": feed_id, **article} for article in articles])
 
-@flow
+@flow(persist_result=False)
 def fetch_rss_articles(id: int):
     url = get_feed_url(id)
     articles = get_feed_articles(url)
     save_articles(id, articles)
-    return articles

0adaf866608a4c09b08a09795d4ec11f238e0362
Parker Brown <97494088+parkerbrown98@users.noreply.github.com>
2024-11-07 18:03:59 -0600

    Refactor audit logic, add logging

--- audit_rss_feeds.py
+++ audit_rss_feeds.py
@@ -5,33 +5,41 @@ from datetime import datetime
 
 import requests
 
-@task(timeout_seconds=10)
-def validate_feed(id: int) -> bool:
-    db_context = SqlAlchemyConnector.load("pg-local")
-    with db_context as session:
-        try:
-            feed = session.fetch_one("SELECT url FROM rss_feeds WHERE id = :id", {"id": id})
-            response = requests.get(feed[0])
-            parser = RSSParser()
-            parser.parse(response.text)
-            return True
-        except Exception as e:
-            return False
- 
+
+@task
+def validate_feed(url: str) -> bool:
+    try:
+        response = requests.get(url, timeout=10)
+        parser = RSSParser()
+        parser.parse(response.text)
+        return True
+    except Exception as e:
+        return False
+
+
 @task(persist_result=False)
 def save_audit_result(id: int, valid: bool):
     db_context = SqlAlchemyConnector.load("pg-local")
     with db_context as session:
-        session.execute("UPDATE rss_feeds SET audited_at = :audited_at, active = :active WHERE id = :id", {"id": id, "audited_at": datetime.now(), "active": valid})
-        
-@flow(persist_result=False)
+        session.execute("UPDATE rss_feeds SET audited_at = :audited_at, active = :active WHERE id = :id", {
+                        "id": id, "audited_at": datetime.now(), "active": valid})
+
+
+@flow(persist_result=False, log_prints=True)
 def audit_rss_feeds(limit: int = 10):
     db_context = SqlAlchemyConnector.load("pg-local")
     with db_context as session:
-        feeds = session.fetch_all("SELECT id FROM rss_feeds ORDER BY audited_at ASC LIMIT :limit", {"limit": limit})
+        feeds = session.fetch_all(
+            "SELECT id, url FROM rss_feeds ORDER BY audited_at ASC LIMIT :limit", {"limit": limit})
+        print(f"Found {len(feeds)} feeds to audit")
+        count = 0
         for feed in feeds:
-            valid = validate_feed(feed[0])
+            valid = validate_feed(feed[1])
             save_audit_result(feed[0], valid)
-            
+            if not valid:
+                count += 1
+        print(f"Disabled {count} feeds due to errors")
+
+
 if __name__ == "__main__":
     audit_rss_feeds()
